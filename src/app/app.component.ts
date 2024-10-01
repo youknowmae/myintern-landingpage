9d1aae5a0de0c061d7a87ba12be07d0922674667
LazyPotatoGamer <krystacoronel13@gmail.com>
2024-10-01 22:33:57 +0800

    Updated UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,27 +5,6 @@ import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterViewInit {
-  @ViewChild('home') homeElement!: ElementRef;
-  @ViewChild('about') aboutElement!: ElementRef;
-  @ViewChild('interface') interfaceElement!: ElementRef;
-  @ViewChild('faculty') facultyElement!: ElementRef;
-  @ViewChild('contact') contactElement!: ElementRef;
+export class AppComponent  {
 
-  private sectionMap = new Map<string, ElementRef>();
-
-  ngAfterViewInit() {
-    this.sectionMap.set('home', this.homeElement);
-    this.sectionMap.set('about', this.aboutElement);
-    this.sectionMap.set('interface', this.interfaceElement);
-    this.sectionMap.set('faculty', this.facultyElement);
-    this.sectionMap.set('contact', this.contactElement);
-  }
-
-  scrollTo(section: string): void {
-    const element = this.sectionMap.get(section);
-    if (element) {
-      element.nativeElement.scrollIntoView({ behavior: 'smooth' });
-    }
-  }
 }
