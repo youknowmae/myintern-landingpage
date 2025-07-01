a72ce48bc2a57e26d910f3651f645d0133c314bd
Raveniis <128274869+Raveniis@users.noreply.github.com>
2025-07-01 12:45:25 +0800

    environment build

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  remove_console_log: false,
+  remove_console_warning: false,
+  remove_console_error: false,
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  remove_console_log: true,
+  remove_console_warning: true,
+  remove_console_error: true,
+};
